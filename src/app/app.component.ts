62290cd73a3b21ac0b7a46be8eff805af4546146
Aaron Emery <Aaron.Emery@acacceptance.com>
2020-08-26 14:14:45 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'NgxMat-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngx-mat-form-validation-library';
+}
